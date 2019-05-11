1efd7b1636ab8560bc0af40a7db529b50f458f44
Anwesh43 <anweshthecool0@gmail.com>
2019-05-11 20:28:50 +0530

    adding DrawingUtil class

--- BiTriFillStage.ts
+++ BiTriFillStage.ts
@@ -32,3 +32,62 @@ class ScaleUtil {
         return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
     }
 }
+
+class DrawingUtil {
+
+      static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number, clip : boolean) {
+          context.beginPath()
+          context.moveTo(x1, y1)
+          context.lineTo(x2, y2)
+          if (!clip) {
+              context.stroke()
+          } else {
+              context.clip()
+          }
+      }
+
+      static drawTriangle(context : CanvasRenderingContext2D, size : number, clip : boolean) {
+          DrawingUtil.drawLine(context, 0, 0, -size / 2, -size / 2, clip)
+          DrawingUtil.drawLine(context, -size / 2, -size / 2, size / 2, -size / 2, clip)
+          DrawingUtil.drawLine(context, size / 2, -size / 2, 0, 0, clip)
+      }
+
+      static drawFillRect(context : CanvasRenderingContext2D, size : number, sc : number) {
+          context.fillRect(-size / 2, -size / 2 * sc, size, size / 2 * sc)
+      }
+
+      static drawFillTriangle(context : CanvasRenderingContext2D, sc : number, size : number) {
+          const x : number = -size * 0.75
+          context.save()
+          DrawingUtil.drawLine(context, 0, 0, x, 0, false)
+          context.translate(x, 0)
+          DrawingUtil.drawTriangle(context, size, false)
+          context.save()
+          DrawingUtil.drawTriangle(context, size, true)
+          DrawingUtil.drawFillRect(context, size, sc)
+          context.restore()
+          context.restore()
+      }
+
+      static drawBTFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+          const w : number = window.innerWidth
+          const h : number = window.innerHeight
+          const gap : number = h / (nodes + 1)
+          const size : number = gap / sizeFactor
+          const sc1 : number = ScaleUtil.divideScale(scale, 0, 2)
+          const sc2 : number = ScaleUtil.divideScale(scale, 1, 2)
+          context.lineCap = 'round'
+          context.lineWidth = Math.min(w, h) / strokeFactor
+          context.strokeStyle = foreColor
+          context.save()
+          context.translate(w / 2, gap * (i + 1))
+          context.rotate(Math.PI / 2 * sc2)
+          for (var j = 0; j < triangles; j++) {
+              context.save()
+              context.scale(1 - 2 * j, 1)
+              DrawingUtil.drawFillTriangle(context, ScaleUtil.divideScale(sc1, j, triangles), size)
+              context.restore()
+          }
+          context.restore()
+      }
+}
